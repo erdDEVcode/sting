6cd0e8f8923f53267fb297189465683fe8d9c612
Ramesh Nair <ram@hiddentao.com>
2020-11-27 14:37:22 +0000

    BREAKING CHANGE: force breaking change to start versioning at 2.0.0

--- src/wdyr.js
+++ src/wdyr.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 if (!window.inProductionMode) {
-    console.log('Setting up why-did-you-render ...')
+    console.log('Setting up why-did-you-render...')
 
     const whyDidYouRender = require('@welldone-software/why-did-you-render')
     
